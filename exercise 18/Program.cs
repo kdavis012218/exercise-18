351e237a8193a0e992c4a70a867fefcf6ff31c53
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-25 22:13:33 -0500

    exercise 18

---
+++ exercise 18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sum();
+            Console.ReadLine();
+
+
+        }
+
+        static void sum()
+        {
+
+
+            int sum;
+            int sol;
+            Console.Write("Enter number:");
+            int.Parse(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Values " + i + ":");
+                sol = int.Parse(Console.ReadLine());
+                (int sum = sum + sol; ++);
+                Console.WriteLine("sum:");
+            Console.WriteLine("Would you like to continue? y or n");
+            Console.ReadLine();
+
+            if input = "y";
+            {
+                return;
+            }
+
+                    else
+                    Console.ReadKey();
+
+        }
+
+            
+        }
+    }
+}
